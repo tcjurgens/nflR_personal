58ffd4823e4bdd7c84a0400cec9ded7dacf9025e
tjurgens22 <72570333+tjurgens22@users.noreply.github.com>
2021-11-02 09:39:48 -0400

    adding columns to the end of the gambling_dataf

--- gambling data/Relevent Gambling DataFrame.R
+++ gambling data/Relevent Gambling DataFrame.R
@@ -9,11 +9,11 @@ load_sharpe_data <- function(file_name) {
   suppressWarnings({ df <- read_csv(url, col_types = cols()) })
   return(df)
 }
-gambling_dataf<-load_sharpe_data("games") %>%
+gambling_dataf <-load_sharpe_data("games") %>% 
   select(season, week, game_type, home_coach, away_coach, home_team, away_team, home_qb_name, away_qb_name,
          home_score, away_score, spread_line, result, home_moneyline, away_moneyline,
-         total_line, total, overtime, home_rest, away_rest,
-         weekday, gametime)
+         total_line, total, overtime, home_rest, away_rest, div_game,
+         weekday, gametime, referee, stadium, roof, surface, temp, wind)
 
 # change gametime to character type column so its easier to filter 
 gambling_dataf$gametime <- as.character(gambling_dataf$gametime)
